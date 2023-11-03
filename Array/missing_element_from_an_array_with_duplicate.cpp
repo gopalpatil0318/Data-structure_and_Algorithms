0c85cd4248bee5f98855e1780301b6db3a2684e3
gopalpatil0318 <gopalpatilrcpit@gmail.com>
2023-11-03 08:11:36 +0530

    Missing element from an array with duplicate

--- Array/missing_element_from_an_array_with_duplicate.cpp
+++ Array/missing_element_from_an_array_with_duplicate.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 // Missing element from an array with duplicate
 
+int sortSwap(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int index = arr[i];
+        swap(arr[i], arr[index - 1]);
+    }
+
+    cout << "missing elements are = ";
+    for (int i = 0; i < n; i++)
+    {
+        if ((i + 1) != arr[i])
+        {
+            cout << i + 1 << " ";
+        }
+    }
+}
+
 int missingEle(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -10,31 +28,28 @@ int missingEle(int arr[], int n)
         int index = abs(arr[i]);
         if (arr[index - 1] > 0)
         {
-            arr[index-1] *= -1;
+            arr[index - 1] *= -1;
         }
     }
 
-
     cout << "missing elements are = ";
-    for(int i =0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        if(arr[i]>0)
+        if (arr[i] > 0)
         {
-            cout << i+1 << " ";
+            cout << i + 1 << " ";
         }
     }
     return 0;
 }
-
-
 
 int main()
 {
-    int arr[] = {1,3,3,4,3};
+    int arr[] = {1, 3, 3, 4, 3};
     int n = 5;
-    missingEle(arr,n);
+    sortSwap(arr, n);
     return 0;
 }
 
-//Output
-// missing elements are = 2 5 
+// Output
+//  missing elements are = 2 5
